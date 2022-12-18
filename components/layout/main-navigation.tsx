8d54cb97353cd3c887f69420d022925da1058280
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-12-18 10:23:38 +0900

    Develop (#6)
    * Feat: page settings (#1)
    
    * Feat: hero 컴포넌트 추가 (#3)
    
    * Feat: layout, navigation add (#5)

---
+++ components/layout/main-navigation.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import classes from './main-naviagtion.module.css';
+import { Logo } from './logo';
+
+export const MainNavigation = () => {
+    return (
+        <header className={classes.header}>
+            <Link href={'/'}>
+                <a>
+                    <Logo />
+                </a>
+            </Link>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href={'/posts'}>Posts</Link>
+                        <Link href={'/contact'}>Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
