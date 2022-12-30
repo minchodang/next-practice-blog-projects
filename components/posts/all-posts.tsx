128cab4f6a625c74f6ead553ca4b97a72c35ed6d
minchodang <minsu910725@gmail.com>
2022-12-30 10:05:21 +0900

    Feat: allPosts 추가

---
+++ components/posts/all-posts.tsx
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+import { PostsGrid } from './posts-grid';
+import { DUMMY_POSTS } from '../../pages';
+
+interface AllPostsProps {
+    posts: DUMMY_POSTS[];
+}
+
+const Posts = styled.section`
+    width: 90%;
+    max-width: 60rem;
+    margin: var(--size-8) auto;
+    h1 {
+        font-size: var(--size-8);
+        color: var(--color-grey-800);
+        text-align: center;
+        @media (min-width: 768px) {
+            font-size: var(--size-16);
+        }
+    }
+`;
+
+export const AllPosts = ({ posts }: AllPostsProps) => {
+    return (
+        <Posts>
+            <h1>All Posts</h1>
+            <PostsGrid posts={posts} />
+        </Posts>
+    );
+};
