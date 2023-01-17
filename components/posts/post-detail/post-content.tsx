d7d5fbaf63df31e1a8b1e61c3b856fffead46834
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-01-17 22:09:20 +0900

    Feat: markDown 스타일링 추가 (#22)

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -2,6 +2,8 @@ import classes from './post-content.module.css';
 import Image from 'next/image';
 import { PostHeader } from './post-header';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 function PostContent(props: { post: any }) {
     const { post } = props;
@@ -38,6 +40,15 @@ function PostContent(props: { post: any }) {
             }
 
             return <p>{paragraph.children}</p>;
+        },
+        code(code: any) {
+            const { className, children } = code;
+            const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+            return (
+                <SyntaxHighlighter style={atomDark} language={language}>
+                    {children}
+                </SyntaxHighlighter>
+            );
         },
     };
 
