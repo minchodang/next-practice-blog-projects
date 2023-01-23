827b1caa753eca01144b2fe44c25e9e26dcc2e4e
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-01-24 07:47:36 +0900

    Feat: contactApi (#26)

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import React, { FormEvent, useState } from 'react';
 
 const Contact = styled.section`
     margin: var(--size-8) auto;
@@ -9,6 +10,7 @@ const Contact = styled.section`
     padding: var(--size-4);
     box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
     font-size: var(--size-6);
+
     h1 {
         font-size: var(--size-8);
         margin: var(--size-4) 0;
@@ -27,6 +29,7 @@ const Form = styled.form`
         font-weight: bold;
         margin: var(--size-2) 0 var(--size-1) 0;
     }
+
     input,
     textarea {
         font: inherit;
@@ -37,6 +40,7 @@ const Form = styled.form`
         background-color: var(--color-grey-50);
         resize: none;
     }
+
     button {
         font: inherit;
         cursor: pointer;
@@ -53,38 +57,73 @@ const Form = styled.form`
     }
 `;
 
-const Controls = styled.form`
+const Controls = styled.div`
     display: flex;
     column-gap: 1rem;
     flex-wrap: wrap;
 `;
-const Control = styled.form`
+const Control = styled.div`
     flex: 1;
     min-width: 10rem;
 `;
-const Actions = styled.form`
+const Actions = styled.div`
     margin-top: var(--size-4);
     text-align: right;
 `;
 
 const ContactForm = () => {
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredMessage, setEnteredMessage] = useState('');
+    const sendMessageHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        fetch(' /api/contact', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: enteredEmail,
+                name: enteredName,
+                message: enteredMessage,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    };
     return (
         <Contact>
             <h1>How can I help you?</h1>
-            <Form>
+            <Form onSubmit={(event) => sendMessageHandler(event)}>
                 <Controls>
                     <Control>
                         <label htmlFor={'email'}>Your Email</label>
-                        <input type={'email'} id={'email'} required />
+                        <input
+                            type={'email'}
+                            id={'email'}
+                            value={enteredEmail}
+                            onChange={(event) => setEnteredEmail(event.target.value)}
+                            required
+                        />
                     </Control>
                     <Control>
                         <label htmlFor={'name'}>Your Name</label>
-                        <input type={'text'} id={'name'} required />
+                        <input
+                            type={'text'}
+                            id={'name'}
+                            value={enteredName}
+                            onChange={(event) => setEnteredName(event.target.value)}
+                            required
+                        />
                     </Control>
                 </Controls>
                 <Control>
                     <label htmlFor={'message'}>Your Message</label>
-                    <textarea id={'message'} rows={5} />
+                    <textarea
+                        id={'message'}
+                        rows={5}
+                        required
+                        value={enteredMessage}
+                        onChange={(event) => setEnteredMessage(event.target.value)}
+                    />
                 </Control>
                 <Actions>
                     <button>Send Message</button>
