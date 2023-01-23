b8dc95aabd992dc4001457eae61b3fae9c8e71db
minchodang <minsu910725@gmail.com>
2023-01-23 09:29:12 +0900

    Feat: contactApi

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,8 +1,5 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 import { AllPosts } from '../../components/posts/all-posts';
-import { getAllPosts, getFeaturedPosts } from '../../lib/post-util';
+import { getAllPosts } from '../../lib/post-util';
 
 export default function AllPostPage(props: any) {
     return <AllPosts posts={props.posts} />;
