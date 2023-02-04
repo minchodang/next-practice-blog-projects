98a2ebffa44d4e82b97dbdcd15275725397c0a11
minchodang <minsu910725@gmail.com>
2023-02-04 16:52:21 +0900

    Feat: head Data 추가

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,8 +1,20 @@
 import { AllPosts } from '../../components/posts/all-posts';
 import { getAllPosts } from '../../lib/post-util';
+import Head from 'next/head';
 
 export default function AllPostPage(props: any) {
-    return <AllPosts posts={props.posts} />;
+    return (
+        <>
+            <Head>
+                <title>All Posts</title>
+                <meta
+                    name={'description'}
+                    content={'A list of all programing-related tutorials and posts!'}
+                />
+            </Head>
+            <AllPosts posts={props.posts} />
+        </>
+    );
 }
 export function getStaticProps() {
     const allPosts = getAllPosts();
